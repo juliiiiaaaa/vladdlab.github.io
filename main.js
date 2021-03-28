3e44a9d400695a059ae94676757dc754ff3e64b0
julia <judithfreaky@gmail.com>
2021-03-28 17:17:17 +0300

    initial commit

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // одержимое тега textarea хранится в его свойстве value
 
 // Подскзка №2: Не забывайте, что LocalStorage и SessionStorage могут хранить только строки в виде пар ключ/значение
-console.log(document.querySelector('textarea').classList)
-
+click = function () {
+    stroka = document.querySelector('#F').value
+    localStorage.setItem(`comment ${localStorage.length + 1}`, stroka)
+    let comment = localStorage.getItem('comment')
+    document.querySelector('.allComments').innerText=(comment)
+}
 
