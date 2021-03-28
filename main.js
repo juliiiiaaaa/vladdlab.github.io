35d7812ba22aecd7b0cc629a87cd2ed1e9874cd8
julia <judithfreaky@gmail.com>
2021-03-28 09:53:25 +0300

    new commit

--- main.js
+++ main.js
@@ -4,4 +4,4 @@
 // Подсказка №2: Не забывайте, что LocalStorage и SessionStorage могут хранить только строки в виде пар ключ/значение
 function cc () {
     console.log(document.querySelector('textarea').classList)
-} 
+}
